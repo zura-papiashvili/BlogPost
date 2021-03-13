d4ae2811cdc7224c515e66bb67fd6cd1164de518
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2021-03-13 21:53:06 +0400

    update as fundraising

--- myproject/models.py
+++ myproject/models.py
@@ -43,6 +43,7 @@ class BlogPost(db.Model):
     title = db.Column(db.String(140),nullable=False)
     text = db.Column(db.Text,nullable=False)
 
+
     def __init__(self,title,text,user_id):
         self.title = title
         self.text = text
